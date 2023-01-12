ac7155d5b8f845b158e76cf3e5e51c6abc2bd047
hajin1015 <10hajin15@gmail.com>
2023-01-12 19:32:07 +0900

    mix content 문제 해결 3차

--- src/read/read.js
+++ src/read/read.js
@@ -24,7 +24,7 @@ const Read = () => {
     }
 
   axios
-    .get("https://52.78.60.246:8080/readRollingpaper",
+    .get("http://52.78.60.246:8080/readRollingpaper",
     {
       params: {
         account_id: accountId,
